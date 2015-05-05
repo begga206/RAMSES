3acfd022b1360af711a57372996fc07a5135f820
Lukas Becker <lukasbecker206@gmail.com>
2015-05-05 11:44:24 +0200

    Commit: Parser Klasse geaddet.

---
+++ RAMSES/src/ramses/Parser.java
@@ -0,0 +1,13 @@
+package ramses;
+
+public class Parser {
+	public Instruction parseInst(String instLine){
+		//TODO
+		return null;
+	}
+	
+	/* TODO
+	 * Instruction parseJump(String instLine)
+	 * Instruction parseCondJump(String instLine)
+	 */
+}
