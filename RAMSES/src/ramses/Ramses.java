25479f7cb0a16ebc845aefbddc446dc6ce01ad8a
Andreas Paul <andreas.paul156@web.de>
2015-05-12 13:38:17 +0200

    Artithmetikbefehle Teil2

--- RAMSES/src/ramses/Ramses.java
+++ RAMSES/src/ramses/Ramses.java
@@ -4,6 +4,7 @@ public class Ramses {
 	public static final int MAX_MEM = 255;
 	public static final int MAX_INDEX = 5;
 	public static final String OUTPUT = "#OUTPUT#";
+	public static final String ERROR_DIVISION_BY_ZERO = "Division by Zero";
 	/** Instruction Counter */
 	private static int counter;
 	/** Instruction Pointer */
@@ -49,53 +50,77 @@ public class Ramses {
 			case DIV_A_MMEM:
 				output += divAMmem(inst);
 				break;
-			case HALT:
+			case HALT: //to do
+				output += halt(inst);
 				return;
-			case IDX_DEC:
+			case IDX_DEC: //to do
+				output += idxDec(inst);
 				break;
-			case IDX_INC:
+			case IDX_INC: //to do
+				output += idxInc(inst);
 				break;
-			case JUMP:
+			case JUMP: //to do
+				output += jump(inst);
 				break;
-			case JUMP_EQ:
+			case JUMP_EQ: //to do
+				output += jumpEq(inst);
 				break;
-			case JUMP_GE:
+			case JUMP_GE: //to do
+				output += jumpGe(inst);
 				break;
-			case JUMP_GT:
+			case JUMP_GT: //to do
+				output += jumpGt(inst);
 				break;
-			case JUMP_LE:
+			case JUMP_LE: //to do
+				output += jumpLe(inst);
 				break;
-			case JUMP_LT:
+			case JUMP_LT: //to do
+				output += jumpLt(inst);
 				break;
-			case JUMP_NE:
+			case JUMP_NE: //to do
+				output += jumpNe(inst);
 				break;
-			case LD_A_MMEM:
+			case LD_A_MMEM: //to do
+				output += ldAMmem(inst);
 				break;
-			case LD_MEM_REG:
+			case LD_MEM_REG: //to do
+				output += ldMemReg(inst);
 				break;
-			case LD_MMEM_A:
+			case LD_MMEM_A: //to do
+				output += ldMmemA(inst);
 				break;
-			case LD_REG_IMM:
+			case LD_REG_IMM: //to do
+				output += ldRegImm(inst);
 				break;
-			case LD_REG_MEM:
+			case LD_REG_MEM: //to do
+				output += ldRegMem(inst);
 				break;
 			case MOD_A_IMM:
+				output += modAImm(inst);
 				break;
 			case MOD_A_MEM:
+				output += modAMem(inst);
 				break;
 			case MOD_A_MMEM:
+				output += modAMmem(inst);
 				break;
 			case MUL_A_IMM:
+				output += mulAImm(inst);
 				break;
 			case MUL_A_MEM:
+				output += mulAMem(inst);
 				break;
 			case MUL_A_MMEM:
+				output += mulAMmem(inst);
 				break;
 			case SUB_A_IMM:
+				output += subAImm(inst);
 				break;
 			case SUB_A_MEM:
+				output += subAMem(inst);
 				break;
 			case SUB_A_MMEM:
+				output += subAMmem(inst);
 				break;
 			default:
 				break;
@@ -128,7 +153,7 @@ public class Ramses {
 			return "\n" + OUTPUT + "a <-- a div " + inst.getP0();
 		}
 		else
-			throw new RuntimeException();
+			throw new RuntimeException(ERROR_DIVISION_BY_ZERO);
 	}
 	
 	private String divAMem(Instruction inst){
@@ -137,7 +162,7 @@ public class Ramses {
 			return "\n" + OUTPUT + "a <-- a div s[" + inst.getP0() + "]";
 		}
 		else
-			throw new RuntimeException();
+			throw new RuntimeException(ERROR_DIVISION_BY_ZERO);
 	}
 	
 	private String divAMmem(Instruction inst){
@@ -146,6 +171,63 @@ public class Ramses {
 			return "\n" + OUTPUT + "a <-- a div s[i" + inst.getP0() + "+" + inst.getP1() + "]";
 		}
 		else
+			throw new RuntimeException(ERROR_DIVISION_BY_ZERO);
+	}
+	
+	private String subAImm(Instruction inst){
+		a -= inst.getP0();
+		return "\n" + OUTPUT + "a <-- a - " + inst.getP0();
+	}
+	
+	private String subAMem(Instruction inst){
+		a -= s[inst.getP0()];
+		return "\n" + OUTPUT + "a <-- a - s[" + inst.getP0() + "]";
+	}
+	
+	private String subAMmem(Instruction inst){
+		a -= s[i[inst.getP0()]+inst.getP1()];
+		return "\n" + OUTPUT + "a <-- a - s[i" + inst.getP0() + "+" + inst.getP1() + "]";
+	}
+	
+	private String modAImm(Instruction inst){
+		if (inst.getP0 != 0){
+			a = a % inst.getP0();
+			return "\n" + OUTPUT + "a <-- a mod " + inst.getP0();
+		}
+		else
 			throw new RuntimeException();
+	}
+	
+	private String modAMem(Instruction inst){
+		if (s[inst.getP0()] != 0){
+			a = a % s[inst.getP0()];
+			return "\n" + OUTPUT + "a <-- a mod s[" + inst.getP0() + "]";
+		}
+		else
+			throw new RuntimeException();
+	}
+	
+	private String modAMmem(Instruction inst){
+		if(s[i[inst.getP0()]+inst.getP1()] != 0){
+			a = a % s[i[inst.getP0()]+inst.getP1()];
+			return "\n" + OUTPUT + "a <-- a mod s[i" + inst.getP0() + "+" + inst.getP1() + "]";
+		}
+		else
+			throw new RuntimeException();
+	}
+	
+	private String mulAImm(Instruction inst){
+		a = a * inst.getP0();
+		return "\n" + OUTPUT + "a <-- a * " + inst.getP0();
+	}
+	
+	private String mulAMem(Instruction inst){
+		a = a * s[inst.getP0()];
+		return "\n" + OUTPUT + "a <-- a * s[" + inst.getP0() + "]";
+	}
+	
+	private String mulAMmem(Instruction inst){
+		a = a * s[i[inst.getP0()]+inst.getP1()];
+		return "\n" + OUTPUT + "a <-- a * s[i" + inst.getP0() + "+" + inst.getP1() + "]";
 	}
 }
